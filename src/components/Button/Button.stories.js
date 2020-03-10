0ced711379cc38fd049f40e0badfc6f53be544e0
vmipfdr <paynemw@yahoo.com>
2020-03-10 11:15:44 -0400

    added a few buttons

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import Button from './Button'
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import Button from "./Button";
 
-storiesOf('Button', module)
-.add('white', () => <Button
-label= 'Submit' 
-/>)
+storiesOf("Button", module)
+  .add("white", () => <Button label="Submit" type="white" />)
+  .add("enter", () => <Button label="Enter at your own Risk" type="enter" />)
+  .add("exit", () => <Button label="Thou Shall Not Pass!" type="exit" />);
--- src/components/Title input/Title.js
+++ src/components/Title input/Title.js
@@ -1,37 +1,45 @@
-import React from 'react';
-import './Title.css';
-
+import React from "react";
+import "./Title.css";
 
 class Title extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        inputValue: ""
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: ""
     };
-}
-render() {  
-    let classes = '';
-    let types = ['text']
+  }
+  render() {
+    let classes = "";
+    let types = ["text"];
     if (types.includes(this.props.type)) {
-        classes += ` Title-${this.props.type}`
+      classes += ` Title-${this.props.type}`;
     }
     if (this.props.Comment) {
-      classes += ` Title-Comment`
+      classes += ` Title-Comment`;
     }
-    let placeholder = this.props.placeholder
-      if (placeholder === "") {
-          placeholder = this.props.label
-      }
-     if (this.props.text) {
-    return (
-      <div>
-        <label className="text-label" htmlFor="input">{this.props.label}</label>
-        <br/>
-        <input onChange={(e) => {this.setState({inputValue: e.target.value})}} className={classes} 
-        type="text" name="input" placeholder={placeholder}/>
-      </div>
-      )
-    };
-};
-};
-    export default Title;
+    let placeholder = this.props.placeholder;
+    if (placeholder === "") {
+      placeholder = this.props.label;
+    }
+    if (this.props.text) {
+      return (
+        <div>
+          <label className="text-label" htmlFor="input">
+            {this.props.label}
+          </label>
+          <br />
+          <input
+            onChange={e => {
+              this.setState({ inputValue: e.target.value });
+            }}
+            className={classes}
+            type="text"
+            name="input"
+            placeholder={placeholder}
+          />
+        </div>
+      );
+    }
+  }
+}
+export default Title;
