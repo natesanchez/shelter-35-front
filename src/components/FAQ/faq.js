9e4536600d38559139467df595d50b4444803280
Nathaniel Sanchez <57163627+natesanchez@users.noreply.github.com>
2020-03-10 18:46:23 -0400

    Revert "merge"

--- src/components/FAQ/faq.js
+++ src/components/FAQ/faq.js
@@ -1,6 +1,6 @@
 import React from "react";
 // import { Link } from "react-router-dom";
-import "./faq.css";
+// import "./Age_restriction.css";
 
 function Agerestrict() {
   return (
@@ -9,34 +9,13 @@ function Agerestrict() {
         <h1>FAQ</h1>
         <div class="container">
           <p>
-           Whats is Shelter35?
+           1
           </p>
           <p>
-           Shelter35 is a place to realese all the tears, fustration, happiness from our evil overlord "Zakk"
+           2
           </p>
           <p>
-
-          </p>
-           <p>
-           Do I need an Internet connection to use Shelter35?
-          </p>
-          <p>
-            Yes you do. Shelter35  is a cloud-based website, meaning that your Meow's are backed up and available wherever you are.
-          </p>
-          <p>
-
-          </p>
-          <p>
-          I'm really offended by that joke you posted!
-          </p>
-          <p>
-          Sorry to hear that. We do hear that, alas, on just about every joke that is even slightly offensive. Please read the Page Warning again.
-          </p>
-           <p>
-           Have a question that's not answered here. What email address should I use to contact you?
-          </p>
-          <p> 
-          Please email nsanchez@usa.com If it’s not a holiday or a weekend, Nate would get back to you pretty slowly. Slower than a Snail.
+           3
           </p>
         </div>
         {/* <div class="button-container">
