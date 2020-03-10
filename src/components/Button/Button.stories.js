a4b60e0d5c2b726b416ba4aef5afa2a2ecf92781
vmipfdr <paynemw@yahoo.com>
2020-03-10 14:22:09 -0400

    making sure i pushed all components

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -5,4 +5,5 @@ import Button from "./Button";
 storiesOf("Button", module)
   .add("white", () => <Button label="Submit" type="white" />)
   .add("enter", () => <Button label="Enter at your own Risk" type="enter" />)
-  .add("exit", () => <Button label="Thou Shall Not Pass!" type="exit" />);
+  .add("exit", () => <Button label="Thou Shall Not Pass!" type="exit" />)
+  .add("edit", () => <Button label="..edit" type="edit" />);
