6e37cfa98b3c041e278717f56f045bbeee21c188
Nate_Sanchez <NSanchez@usa.com>
2020-03-12 13:48:08 -0400

    commit

--- src/components/FAQ/faq.js
+++ src/components/FAQ/faq.js
@@ -1,52 +1,49 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "./faq.css";
-import Button from "../Button/Button.js"
+import Button from "../Button/Button.js";
 
 class faq extends Component {
-  render () {
+  render() {
     return (
       <div className="faqTopMain">
-  
         <div class="faqinformative">
           <h1>FAQ</h1>
           <div class="faqcontainer">
+            <p className="ul">Whats is Shelter35?</p>
+            <p className="li">
+              Shelter35 is a place to release all the tears, fustration,
+              happiness from our 12 week experience.
+            </p>
             <p className="ul">
-             Whats is Shelter35?
+              Do I need an Internet connection to use Shelter35?
             </p>
             <p className="li">
-             Shelter35 is a place to realese all the tears, fustration, happiness from our evil overlord "Zakk"
+              Yes you do. Shelter35 is a cloud-based website, meaning that your
+              Meow's are backed up and available wherever you are.
             </p>
             <p className="ul">
-             Do I need an Internet connection to use Shelter35?
+              Have a question that's not answered here. What email address
+              should I use to contact you?
             </p>
             <p className="li">
-              Yes you do. Shelter35  is a cloud-based website, meaning that your Meow's are backed up and available wherever you are.
-            </p>
-            <p className="ul">
-            I'm really offended by that joke you posted!
-            </p>
-            <p className="li">
-            Sorry to hear that. We do hear that, alas, on just about every joke that is even slightly offensive. Please read the Page Warning again.
-            </p>
-            <p className="ul">
-             Have a question that's not answered here. What email address should I use to contact you?
-            </p>
-            <p className="li"> 
-            Please email nsanchez@usa.com If it’s not a holiday or a weekend, Nate would get back to you pretty slowly. Slower than a Snail.
+              Please email nsanchez@usa.com If it’s not a holiday or a weekend,
+              Nate will get back to you slowly. Like really slow.
             </p>
           </div>
+          <br />
+          <br />
+          <br />
+          <br />
           <div className="button">
-          <Link className="enterHome" to={"/home"}>
-            <Button label="Home" type="enter" />
-          </Link>
-        </div>
+            <Link className="enterHome" to={"/home"}>
+              <Button label="Home" type="enter" />
+            </Link>
+          </div>
         </div>
       </div>
     );
   }
 }
-
-
 
 export default faq;
