14ce678b3e1fd5b90dbc004143002666a8d4ea8d
Nate_Sanchez <NSanchez@usa.com>
2020-03-13 09:04:16 -0400

    commit

--- src/components/FAQ/faq.js
+++ src/components/FAQ/faq.js
@@ -3,31 +3,6 @@ import { Link } from "react-router-dom";
 import "./faq.css";
 import Button from "../Button/Button.js";
 
-function faq() {
-  return (
-    <div className="TopMain">
-      <div class="informative">
-        <h1>FAQ</h1>
-        <div class="container">
-          <ul>What is Shelter35?</ul>
-          <li>
-            Shelter35 is a place to release all the tears, fustration, happiness
-            from our 12 week experience.
-          </li>
-          <ul>Do I need an Internet connection to use Shelter35?</ul>
-          <li>
-            Yes you do. Shelter35 is a cloud-based website, meaning that your
-            Meow's are backed up and available wherever you are.
-          </li>
-          <ul>
-            Have a question that's not answered here. What email address should
-            I use to contact you?
-          </ul>
-          <li>
-            Please email nsanchez@usa.com If it’s not a holiday or a weekend,
-            Nate will get back to you slowly. Like really slow.
-          </li>
-    
 class faq extends Component {
   render() {
     return (
